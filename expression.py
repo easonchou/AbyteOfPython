05988933bb827437f6bd12b65e76391d8b34ed81
easonchou <eason.chou@qq.com>
2016-04-09 10:30:13 +0800

    new

---
+++ expression.py
@@ -0,0 +1,7 @@
+length = 5
+breadth = 2
+
+print('Area is ', area)
+print('Perimeter is ', 2 * (length + breadth))
+print('area',0)
+print(length)
---
+++ var.py
@@ -0,0 +1,18 @@
+# Filename : var.py
+i = 5
+print(i)
+i = i + 1
+print(i)
+s = '''This is a multi-line string.
+This is the second line.'''
+print(s)
+
+i = 5
+print i
+
+print \
+i
+print i
+
+b = 5 // 3
+print ("b = {0}".format(b))
